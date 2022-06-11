86a3087d75c5c94a748bb1cdf14a71d26c57c583
carnoldcoding <carnoldcoding@gmail.com>
2022-06-11 15:59:41 -0400

    some functionality

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
     ===LIBRARY APP===
     Created by: Cameron Arnold
 */
+
+//Classes
+function Book(title, author, pages) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages
+}
+
+function Library(){
+    this.catalogue = []
+    this.addToLibrary = function(){
+        const testBook = new Book("Lord of the Rings", "J.R Tolkein", 0);
+        this.catalogue.push(testBook);
+    }
+    this.displayLibrary = function(){
+    }
+}
