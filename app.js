630e1dcd97563ffbbabb93d3e12618237406808a
carnoldcoding <carnoldcoding@gmail.com>
2022-06-16 19:54:26 -0400

    card expand

--- app.js
+++ app.js
@@ -12,10 +12,31 @@ function Book(title, author, pages) {
 
 function Library(){
     this.catalogue = []
+
     this.addToLibrary = function(){
         const testBook = new Book("Lord of the Rings", "J.R Tolkein", 0);
         this.catalogue.push(testBook);
     }
-    this.displayLibrary = function(){
+
+    this.removeFromLibrary = function(bookToRemove){
+        this.catalogue = this.catalogue.filter((book) => {
+            book.title == bookToRemove.title;
+        })
     }
+}
+
+const library = new Library();
+library.addToLibrary();
+
+/*
+    ==Debugging==
+    Create card grid
+*/
+
+const cardGrid = document.querySelector(".card-grid");
+const cardCount = 0;
+for(let i=0; i<cardCount; i++){
+    let card = document.createElement("div");
+    card.classList.add("card");
+    cardGrid.appendChild(card);
 }
